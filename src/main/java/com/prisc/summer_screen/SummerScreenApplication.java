c31250592202bd5da6a5feb229c0c6363a8ae91e
pris-c <priscila.campoos@yahoo.com.br>
2025-02-20 17:41:24 +0000

    Setup project

---
+++ src/main/java/com/prisc/summer_screen/SummerScreenApplication.java
@@ -0,0 +1,13 @@
+package com.prisc.summer_screen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SummerScreenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SummerScreenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/prisc/summer_screen/SummerScreenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prisc.summer_screen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SummerScreenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
