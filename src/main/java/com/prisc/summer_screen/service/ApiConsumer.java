e16c30b883839b06d45bee767ced7e0acabc8a18
pris-c <priscila.campoos@yahoo.com.br>
2025-02-21 15:31:51 +0000

    Add documentation

--- src/main/java/com/prisc/summer_screen/service/ApiConsumer.java
+++ src/main/java/com/prisc/summer_screen/service/ApiConsumer.java
@@ -6,8 +6,19 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
+
+/**
+ * A simple API consumer that fetches data from a given URL using Java's HttpClient.
+ */
 public class ApiConsumer {
 
+    /**
+     * Sends an HTTP GET request to the specified URL and returns the response body as a String.
+     *
+     * @param path The URL to send the request to.
+     * @return The response body as a String.
+     * @throws RuntimeException If an IOException or InterruptedException occurs during the request.
+     */
     public String getData(String path){
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
--- src/main/java/com/prisc/summer_screen/service/OmdbApiConsumer.java
+++ src/main/java/com/prisc/summer_screen/service/OmdbApiConsumer.java
@@ -1,6 +1,17 @@
 package com.prisc.summer_screen.service;
 
+/**
+ * A specialized API consumer for fetching movie data from the OMDb API.
+ * Extends {@link ApiConsumer} to reuse its HTTP request functionality.
+ */
 public class OmdbApiConsumer extends ApiConsumer {
+
+    /**
+     * Fetches series or movie data from the OMDb API using the given the title.
+     *
+     * @param title The title of the movie or series to search for.
+     * @return The JSON response from the OMDb API as a String.
+     */
     public String getData(String title){
         title = title.replace(" ", "+");
         String endpoint = "https://www.omdbapi.com/?t=" +
