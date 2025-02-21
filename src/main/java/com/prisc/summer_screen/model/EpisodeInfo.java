e566b31f37c3d449f8bf704f4e27f57e925f1fbf
pris-c <priscila.campoos@yahoo.com.br>
2025-02-21 17:27:06 +0000

    Documentation

--- src/main/java/com/prisc/summer_screen/model/EpisodeInfo.java
+++ src/main/java/com/prisc/summer_screen/model/EpisodeInfo.java
@@ -3,6 +3,15 @@ package com.prisc.summer_screen.model;
 import com.fasterxml.jackson.annotation.JsonAlias;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+/**
+ * A record representing information about a specific episode of a TV series.
+ * This class is used to map JSON responses, typically from sources like the OMDb API.
+ *
+ * @param title      The title of the episode, mapped from the JSON field "Title".
+ * @param episode    The episode number, mapped from the JSON field "Episode".
+ * @param rating     The IMDb rating of the episode, mapped from the JSON field "imdbRating".
+ * @param releaseDate The release date of the episode, mapped from the JSON field "Released".
+ */
 @JsonIgnoreProperties(ignoreUnknown = true)
 public record EpisodeInfo(
         @JsonAlias("Title") String title,
--- src/main/java/com/prisc/summer_screen/model/SeasonInfo.java
+++ src/main/java/com/prisc/summer_screen/model/SeasonInfo.java
@@ -5,6 +5,13 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import java.util.List;
 
+/**
+ * A record representing information about a TV series season.
+ * This class is used to map JSON responses from the OMDb API.
+ *
+ * @param season   The season number, mapped from the JSON field "Season".
+ * @param episodes The list of episodes in the season, mapped from the JSON field "Episodes".
+ */
 @JsonIgnoreProperties(ignoreUnknown = true)
 public record SeasonInfo(
         @JsonAlias("Season")  Integer season,
